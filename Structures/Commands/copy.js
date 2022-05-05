49a691dec6fa1c78d388aff70c20042e7753dc98
Edemarz <edemarzfn@gmail.com>
2022-05-05 13:47:07 +0700

    Update#6

--- Structures/Commands/copy.js
+++ Structures/Commands/copy.js
@@ -9,6 +9,7 @@ function run({ args, chat }) {
     if (!args[0]) return help({ args, chat });
     if (isNaN(args[0]) && typeof args !== 'number') return chat.chat("&6[HSBC]&r&c The argument must be a Number!&r");
     const index = Number(args);
+    if (index === 0) return chat.chat("&6[HSBC]&r&c You have to input number above 0!")
     const storageLength = Storage.messages.length;
 
     if (index > storageLength) return chat.chat(`&6[HSBC]&r&c You cannot copy that message since it is more than how much message HSBC has stored!`);
